0299a74de50496763a13568c75878f170c751bee
HelloKitty <andrew.blakely@ymail.com>
2021-01-21 02:51:40 -0600

    Added screen constants type

---
+++ src/Rs317.Library.Client.API/Rendering/ScreenConstants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317
+{
+	/// <summary>
+	/// The RS screen constants.
+	/// </summary>
+	public static class ScreenConstants
+	{
+		/// <summary>
+		/// The default RS screen width.
+		/// </summary>
+		public const int DEFAULT_SCREEN_WIDTH = 765;
+
+		/// <summary>
+		/// The default RS screen height.
+		/// </summary>
+		public const int DEFAULT_SCREEN_HEIGHT = 503;
+
+		/// <summary>
+		/// The default RS screen aspect ratio.
+		/// </summary>
+		public const float DEFAULT_SCREEN_ASPECT_RATIO = (float)765 / 503;
+	}
+}
